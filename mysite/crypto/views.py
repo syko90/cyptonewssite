da2f9cbc323bb3d59d0750ff23638dc5e7345570
psycho <kurantowicz.maciej@gmail.com>
2020-02-17 23:16:54 +0100

    9# getting price of crypto from api

--- mysite/crypto/views.py
+++ mysite/crypto/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+
 def home(request):
     import json
     import requests
+
+    # Grab some Price Data
+    price_request =  requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP&tsyms=USD')
+    price = json.loads(price_request.content)
+
+
+    # Grab some crypto news
     api_request =  requests.get('https://min-api.cryptocompare.com/data/v2/news/?lang=EN')
     api = json.loads(api_request.content)
-    return render(request, 'home.html', {'api':api })
+    return render(request, 'home.html', {'api':api, 'price': price })
